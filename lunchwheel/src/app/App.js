fbf35be513b9b40db3cb528d7a353e166c64ee1f
Sarah Spriggs <sarah1591@gmail.com>
2020-09-28 22:37:02 -0400

    Continued setting up project structure. Removed unused/duplicate files. Added SASS and recharts.

---
+++ lunchwheel/src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>I am a header.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
